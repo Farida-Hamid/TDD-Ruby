de8aa6d7a06a7ec303ab0d1056fb036b64e4c403
Farida-Hamid <faridaaddala@gmail.com>
2022-11-24 20:59:48 +0300

    Fix linters errors

--- solver.rb
+++ solver.rb
@@ -1,20 +1,19 @@
 class Solver
-  def factorial(n)
-    raise "Please enter a positive number" if n < 0
+  def factorial(num)
+    raise 'Please enter a positive number' if num.negative?
 
-    (1..n).inject(:*) || 1
+    (1..num).inject(:*) || 1
   end
 
   def reverse(str)
     str.reverse
   end
 
-  def fizzbuzz(n)
-    output = ""
-    output = "Fizz" if (n%3).zero?
-    output += "Buzz" if (n%5).zero?
-    output = n.to_s if output.empty?
+  def fizzbuzz(num)
+    output = ''
+    output = 'Fizz' if (num % 3).zero?
+    output += 'Buzz' if (num % 5).zero?
+    output = num.to_s if output.empty?
     output
   end
 end
-
