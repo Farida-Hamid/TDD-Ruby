7692b49f2354d4b1f8a6361a1100694a58c95f81
Farida-Hamid <faridaaddala@gmail.com>
2022-11-24 20:45:49 +0300

    Build the fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -9,7 +9,12 @@ class Solver
     str.reverse
   end
 
-  def fizzbuzz
+  def fizzbuzz(n)
+    output = ""
+    output = "Fizz" if (n%3).zero?
+    output += "Buzz" if (n%5).zero?
+    output = n.to_s if output.empty?
+    output
   end
 end
 
