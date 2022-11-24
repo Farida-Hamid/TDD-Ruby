9c786900e47a47275bc43a2229455f0ce6a893cc
Farida-Hamid <faridaaddala@gmail.com>
2022-11-24 18:17:08 +0300

    Build tests for the factorial method

--- rspec/solver_spec.rb
+++ rspec/solver_spec.rb
@@ -1,11 +1,26 @@
+require '../solver'
+
 describe "Test the solver class:" do
 
-  it "Test the factorial method:" do
+  context "Test the factorial method:" do
+    result = Solver.new
+
+    it "The factorial of 6 should be 720" do
+      expect(result.factorial(6)).to eql 720
+    end
+
+    it "The factorial of 0 should be 1" do
+      expect(result.factorial(0)).to eql 1
+    end
+
+    it "The factorial of 0 should be 1" do
+      expect{result.factorial(-1)}.to raise_error("Please enter a positive number")
+    end
   end
   
-  it "Test the reverse method:" do
+  xit "Test the reverse method:" do
   end
 
-  it "Test the fizzbuzz method:" do
+  xit "Test the fizzbuzz method:" do
   end
 end
