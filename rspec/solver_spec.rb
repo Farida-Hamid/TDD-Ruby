e92ed214f0c4ab71e574974910b74080d7174ac8
Farida-Hamid <faridaaddala@gmail.com>
2022-11-24 17:16:45 +0300

    Create the kelaton for class solver and tests

---
+++ rspec/solver_spec.rb
@@ -0,0 +1,11 @@
+describe "Test the solver class:" do
+
+  it "Test the factorial method:" do
+  end
+  
+  it "Test the reverse method:" do
+  end
+
+  it "Test the fizzbuzz method:" do
+  end
+end
